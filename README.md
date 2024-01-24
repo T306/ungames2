3b54e343c1c20404010839f7d3b5ed92ff463ab1
T306Dev <57376135+T306@users.noreply.github.com>
2024-01-23 22:15:34 -0500

    - upd README.md - change .ignores - formatting

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# ungames2
-A gaming site made using quart and bootstrap that can be deployed using docker.
+
+# UnGames2  
+#### A gaming site made using quart and bootstrap that can be deployed using docker.
+
+Todo:
+ - [ ] Add a way to edit game descriptions
+ - [ ] Make better documentation
+ - [ ] Change SECRET_KEY solution
+ - [ ] Create branding 
+ - [ ] Any other features that I think of
--- dbgen.py
+++ dbgen.py
@@ -7,63 +7,52 @@ def title_gen(f):
     name = f.replace('Games\\', '')
     name = name.replace('.swf', '')
     name = name.replace('-', ' ')
-    return name
+    return name.title()
 
 
-gameList = []
-game2Dict = []
-directory = 'Games'
+def main():
+    game_list = []
+    game2dict = []
 
-if os.path.exists("identifier.sqlite"):
     connection = sqlite3.connect("identifier.sqlite")
     cursor = connection.cursor()
     statement = "INSERT INTO Games VALUES (?, ?, ?, ?)"
-else:
-    connection = sqlite3.connect("identifier.sqlite")
-    cursor = connection.cursor()
-    cursor.execute("CREATE TABLE Games (title TEXT, description TEXT, file TEXT, image TEXT)")
-    statement = "INSERT INTO Games VALUES (?, ?, ?, ?)"
 
-# noinspection SqlWithoutWhere
-cursor.execute("DELETE FROM Games;")
+    cursor.execute("drop table if exists Games")
+    cursor.execute("create table Games (title TEXT, description TEXT, file TEXT, image TEXT)")
 
-# Implement natsort
-for game in os.listdir('Games'):
-    temp = game.replace('.swf', '')
-    game = temp.replace('-', ' ')
-    gameList.append(game)
+    # Implement natsort
+    for game in os.listdir('Games'):
+        game = game.replace('.swf', '').replace('-', ' ')
+        game_list.append(game)
 
-gameList = natsorted(gameList)
+    game_list = natsorted(game_list)
 
-# print(gameList)
+    for game in game_list:
+        index = game_list.index(game)
+        game = game.replace(' ', '-') + '.swf'
+        game_list[index] = game
 
-for game in gameList:
-    index = gameList.index(game)
-    game = game.replace(' ', '-')
-    game = game + '.swf'
-    gameList[index] = game
+    # Converts games to dict
+    for game in game_list:
+        game_name = title_gen(game)
+        # game_desc = game_name
+        game_img = game.replace(' ', '-').replace('.swf', '') + '.webp'
+        game_file = '/Games/' + game
+        game = dict(title=game_name, description=game_name, image=game_img, file=game_file)
+        game2dict.append(game)
 
-# print(gameList)
+    del game_list
 
-# Converts games to dict
-for game in gameList:
-    # print(game)
-    game_name = title_gen(game).title()
-    game_desc = game_name
-    game_img = game.replace(' ', '-')
-    game_img = game_img.replace('.swf', '') + '.webp'
-    game_file = '/Games/' + game
-    # print(game_file)
-    game = dict(title=game_name, description=game_desc, image=game_img, file=game_file)
-    game2Dict.append(game)
+    for x in game2dict:
+        file = x['file'].replace(' ', '-') + '.swf'
+        cursor.execute(statement, (x['title'], x['description'], x['image'], file))
 
-# print(game2Dict)
+    connection.commit()
+    connection.close()
 
-for x in game2Dict:
-    file = x['file'].replace(' ', '-') + '.swf'
-    cursor.execute(statement, (x['title'], x['description'], x['image'], file))
 
-connection.commit()
-connection.close()
-
-print("Games Database Generated")
+if __name__ == "__main__":
+    print("Generating Games Database ")
+    main()
+    print("Games Database Generated!!")
